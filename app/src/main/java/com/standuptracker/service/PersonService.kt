f16de957a606cc6e787f4422a90efe02abbcce27
jthakur2023 <thakurj2000@gmail.com>
2020-07-05 22:09:25 -0400

    Added the Log.d("JSON", "Save Failed") first

--- app/src/main/java/com/standuptracker/service/PersonService.kt
+++ app/src/main/java/com/standuptracker/service/PersonService.kt
@@ -10,6 +10,7 @@ import retrofit2.Callback
 import retrofit2.Response
 import java.lang.Exception
 
+
 class PersonService {
     fun fetchPeople() : MutableLiveData<ArrayList<Person>> {
         val people = MutableLiveData<ArrayList<Person>>()
@@ -18,8 +19,8 @@ class PersonService {
         // must put call on the background thread
         call?.enqueue(object: Callback<ArrayList<Person>> {
             override fun onFailure(call: Call<ArrayList<Person>>, t: Throwable) {
-                throw Exception("failed to read JSON")
                 Log.d("JSON", "Save Failed")
+                throw Exception("failed to read JSON")
             }
 
             override fun onResponse(
