77818d1fe978fcef303bc17a31da5f42110e54d2
Anne Catherwood <annecatherwood@gmail.com>
2020-07-14 22:06:54 -0400

    add user auth

--- app/src/main/java/com/standuptracker/ui/home/HomeFragment.kt
+++ app/src/main/java/com/standuptracker/ui/home/HomeFragment.kt
@@ -1,17 +1,25 @@
 package com.standuptracker.ui.home
 
+import android.app.Activity.RESULT_OK
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.standuptracker.R
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
 
+    private val AUTH_REQUEST_CODE = 2002
+    private var user : FirebaseUser? = null
     private lateinit var homeViewModel: HomeViewModel
 
     override fun onCreateView(
@@ -27,5 +35,31 @@ class HomeFragment : Fragment() {
             textView.text = it
         })
         return root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        btnLogon.setOnClickListener{
+            logon()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK){
+            if (requestCode == AUTH_REQUEST_CODE) {
+                user = FirebaseAuth.getInstance().currentUser
+            }
+        }
+    }
+
+    private fun logon() {
+        var providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build()
+        )
+        startActivityForResult(
+            AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(providers).build(), AUTH_REQUEST_CODE
+        )
     }
 }
