e8876928823e4813c0a1edff7bfb9bcb4cc5f503
Anne Catherwood <annecatherwood@gmail.com>
2020-07-17 21:32:46 -0400

    improves UI and adds calendar view popup on click of textview

--- app/src/main/java/com/standuptracker/ui/home/HomeFragment.kt
+++ app/src/main/java/com/standuptracker/ui/home/HomeFragment.kt
@@ -1,11 +1,13 @@
 package com.standuptracker.ui.home
 
 import android.app.Activity.RESULT_OK
+import android.app.DatePickerDialog
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.DatePicker
 import android.widget.TextView
 import com.firebase.ui.auth.AuthUI
 import com.google.firebase.auth.FirebaseAuth
@@ -15,12 +17,15 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.standuptracker.R
 import kotlinx.android.synthetic.main.fragment_home.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class HomeFragment : Fragment() {
 
     private val AUTH_REQUEST_CODE = 2002
     private var user : FirebaseUser? = null
     private lateinit var homeViewModel: HomeViewModel
+    var cal = Calendar.getInstance()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -42,6 +47,28 @@ class HomeFragment : Fragment() {
         btnLogon.setOnClickListener{
             logon()
         }
+
+        // create an OnDateSetListener
+        val dateSetListener = object : DatePickerDialog.OnDateSetListener {
+            override fun onDateSet(view: DatePicker, year: Int, monthOfYear: Int,
+                                   dayOfMonth: Int) {
+                cal.set(Calendar.YEAR, year)
+                cal.set(Calendar.MONTH, monthOfYear)
+                cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+                updateDateInView()
+            }
+        }
+
+        txtDate.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(view: View) {
+                activity?.let {
+                    DatePickerDialog(
+                        it, dateSetListener, // set DatePickerDialog to point to today's date when it loads up
+                        cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH)).show()
+                }
+            }
+
+        })
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -51,6 +78,12 @@ class HomeFragment : Fragment() {
                 user = FirebaseAuth.getInstance().currentUser
             }
         }
+    }
+
+    private fun updateDateInView() {
+        val myFormat = "MM/dd/yyyy" // mention the format you need
+        val sdf = SimpleDateFormat(myFormat, Locale.US)
+        txtDate!!.text = sdf.format(cal.getTime())
     }
 
     private fun logon() {
